b933d0fc0a77a2bab2bdb8ce89707131b317e1f7
Jens Drenhaus <jens.drenhaus@9elements.com>
2021-05-11 14:41:15 +0200

    add LICENSE file
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

--- pkg/stboot/signature_test.go
+++ pkg/stboot/signature_test.go
@@ -1,1 +1,5 @@
+// Copyright 2021 the System Transparency Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package stboot
--- tools/stmanager/stmanager.go
+++ tools/stmanager/stmanager.go
@@ -1,3 +1,7 @@
+// Copyright 2021 the System Transparency Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 // stconfig is a configuration tool to create and manage artifacts for

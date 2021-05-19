e8d0eb2c511682d6fe77a85d67baeeb771281b06
Jens Drenhaus <jens.drenhaus@9elements.com>
2021-05-19 19:11:59 +0200

    add stlog package
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

---
+++ stlog/std.go
@@ -0,0 +1,56 @@
+// Copyright 2021 the System Transparency Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package stlog
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+type standardLogger struct {
+	out   *log.Logger
+	level LogLevel
+}
+
+func newStandardLogger() *standardLogger {
+	sl := log.New(os.Stderr, "", 0)
+	return &standardLogger{
+		out:   sl,
+		level: DebugLevel,
+	}
+}
+
+func (l *standardLogger) setLevel(level LogLevel) {
+	l.level = level
+}
+
+func (l *standardLogger) error(format string, v ...interface{}) {
+	if l.level >= ErrorLevel {
+		msg := errorTag + prefix + fmt.Sprintf(format, v...)
+		l.out.Print(msg)
+	}
+}
+
+func (l *standardLogger) warn(format string, v ...interface{}) {
+	if l.level >= WarnLevel {
+		msg := warnTag + prefix + fmt.Sprintf(format, v...)
+		l.out.Print(msg)
+	}
+}
+
+func (l *standardLogger) info(format string, v ...interface{}) {
+	if l.level >= InfoLevel {
+		msg := infoTag + prefix + fmt.Sprintf(format, v...)
+		l.out.Print(msg)
+	}
+}
+
+func (l *standardLogger) debug(format string, v ...interface{}) {
+	if l.level >= DebugLevel {
+		msg := debugTag + prefix + fmt.Sprintf(format, v...)
+		l.out.Print(msg)
+	}
+}
---
+++ stlog/syslog.go
@@ -0,0 +1,58 @@
+// Copyright 2021 the System Transparency Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package stlog
+
+import (
+	"fmt"
+
+	"github.com/u-root/u-root/pkg/ulog"
+)
+
+type kernelLogger struct {
+	out   *ulog.KLog
+	level LogLevel
+}
+
+func newKernlLogger() *kernelLogger {
+	kl := ulog.KernelLog
+	kl.SetLogLevel(ulog.KLogNotice)
+	kl.SetConsoleLogLevel(ulog.KLogInfo)
+	return &kernelLogger{
+		out:   kl,
+		level: DebugLevel,
+	}
+}
+
+func (l *kernelLogger) setLevel(level LogLevel) {
+	l.level = level
+}
+
+func (l *kernelLogger) error(format string, v ...interface{}) {
+	if l.level >= ErrorLevel {
+		msg := errorTag + prefix + fmt.Sprintf(format, v...)
+		l.out.Print(msg)
+	}
+}
+
+func (l *kernelLogger) warn(format string, v ...interface{}) {
+	if l.level >= WarnLevel {
+		msg := warnTag + prefix + fmt.Sprintf(format, v...)
+		l.out.Print(msg)
+	}
+}
+
+func (l *kernelLogger) info(format string, v ...interface{}) {
+	if l.level >= InfoLevel {
+		msg := infoTag + prefix + fmt.Sprintf(format, v...)
+		l.out.Print(msg)
+	}
+}
+
+func (l *kernelLogger) debug(format string, v ...interface{}) {
+	if l.level >= DebugLevel {
+		msg := debugTag + prefix + fmt.Sprintf(format, v...)
+		l.out.Print(msg)
+	}
+}

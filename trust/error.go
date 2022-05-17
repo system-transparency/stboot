2fceed6b4054a9d05b71071d6d1d7858835be52b
Jens Drenhaus <jens.drenhaus@9elements.com>
2022-05-17 16:26:56 +0200

    trust: rework errors
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

--- trust/error.go
+++
@@ -1,9 +0,0 @@
-package trust
-
-// Error reports problems regarding signature verification.
-type Error string
-
-// Error implements error interface.
-func (e Error) Error() string {
-	return string(e)
-}
